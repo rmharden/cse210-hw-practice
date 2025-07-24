2834acb1c6cb217a97ffb2ef95313eb29c2c826c
rmharden <har22079@byui.edu>
2025-07-24 15:18:15 -0400

    made a conditional while loop.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,9 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        // Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
+        string _userResponse = "";
         Word w = new Word("truth");
-        Console.WriteLine($"\n{w.GetDisplayText()}");
+        while (w.IsHidden() && _userResponse.ToLower() != "quit")
+        {
+            Console.Write($"\n{w.GetDisplayText()}");
 
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+            _userResponse = Console.ReadLine();
+
+        }
+        
     }
 }
