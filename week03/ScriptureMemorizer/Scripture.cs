7297c260e0a4e37c9fbf356cbc48f5cfa86a8fae
rmharden <har22079@byui.edu>
2025-07-24 11:51:11 -0400

    made the member variables in Scripture private

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -1,7 +1,7 @@
 public class Scripture
 {
-    Reference _reference;
-    List<Word> _words;
+    private Reference _reference;
+    private List<Word> _words;
     public Scripture(Reference reference, string text)
     {
         _reference = reference;
