cb4ebbf2c4342c1260a26e2eac3cf58e1b8a86c1
rmharden <har22079@byui.edu>
2025-07-27 20:09:34 -0400

    still working to find a solution to make the list unique for each time.

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -17,14 +17,36 @@ public class PromptGenerator
         // return _prompts[i];
 
         // Trying to make a random prompt that is different each time:
+
+        // This option works, but once the count gets to 0, there are no prompts for the zero... so how do you restart the count i the user presses 1 from the menu again?
+        // Random r = new Random();
+        // while (_prompts.Count > 0)
+        // {
+        //     int i = r.Next(_prompts.Count);
+        //     string selectedPrompt = _prompts[i];
+        //     _prompts.RemoveAt(i);
+        //     return selectedPrompt;
+        // }
+        // return "";
+
+        // this method doesn't work:
+        // trying another way:
+        // Random r = new Random();
+        // List<string> _uniquePrompts = new List<string>(_prompts);
+        // if (_uniquePrompts.Count == 0)
+        // {
+        //     _uniquePrompts = new List<string>(_prompts);
+        // }
+        // int i = r.Next(_uniquePrompts.Count);
+        // string prompt = _uniquePrompts[i];
+        // _uniquePrompts.RemoveAt(i);
+        // return prompt;
+
+        // Try to combine the two:
         Random r = new Random();
-        while (_prompts.Count > 0)
-        {
-            int i = r.Next(_prompts.Count);
-            string selectedPrompt = _prompts[i];
-            _prompts.RemoveAt(i);
-            return selectedPrompt;
-        }
-        return "";
+        
     }
 }
+// https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-9.0
+// https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.removeat?view=net-9.0
+// https://learn.microsoft.com/en-us/answers/questions/1661541/add-items-to-list-randomly
