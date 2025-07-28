9477d62353dcf99740c2c67da651a35ad0e14856
rmharden <har22079@byui.edu>
2025-07-28 17:54:39 -0400

    wrote a while loop to clear the screen and display the scripture and quit when the user types quit

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,27 +1,17 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
         string _userResponse = "";
-        Word w = new Word("truth");
-        Reference r1 = new Reference("John", 3, 16);
-        Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-        while (s1.IsCompletelyHidden() && _userResponse.ToLower() != "quit")
+
+        while (_userResponse.ToLower() != "quit")
         {
             Console.Clear();
+            Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.\n");
 
-            // Console.WriteLine(r1.GetDisplayText());
-            Console.Write(s1.GetDisplayText());
-
-            Console.Write($"{w.GetDisplayText()}");
-
-            Console.WriteLine(" ");
-            Console.WriteLine("\nPress enter to continue or type 'quit' to finish:");
+            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             _userResponse = Console.ReadLine();
         }
-        
     }
 }
