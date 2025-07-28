b94176ac082447d2ba06c165726a6f6cf0341227
rmharden <har22079@byui.edu>
2025-07-28 18:18:08 -0400

    The Reference test works now

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -8,7 +8,14 @@ class Program
         while (_userResponse.ToLower() != "quit")
         {
             Console.Clear();
-            Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.\n");
+            Reference r1 = new Reference("John", 3, 16);
+            Console.WriteLine(r1.GetDisplayText());
+
+            Reference r2 = new Reference("Proverbs", 3, 5, 6);
+            Console.WriteLine(r2.GetDisplayText());
+
+
+            //Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.\n");
 
             Console.WriteLine("Press enter to continue or type 'quit' to finish:");
             _userResponse = Console.ReadLine();
