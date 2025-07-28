2a00d24d06e7f260cc1587272e5ac1317d591ee6
rmharden <har22079@byui.edu>
2025-07-27 20:28:46 -0400

    took out a using

--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -11,14 +11,16 @@ public class PromptGenerator
     public string GetRandomPrompt()
     {
         // This works:
-        // Random r = new Random();
-        // //get a random index value and lookup the string in _prompts
-        // int i = r.Next(_prompts.Count);
-        // return _prompts[i];
+        Random r = new Random();
+        //get a random index value and lookup the string in _prompts
+        int i = r.Next(_prompts.Count);
+        return _prompts[i];
+    }
+}
 
         // Trying to make a random prompt that is different each time:
 
-        // This option works, but once the count gets to 0, there are no prompts for the zero... so how do you restart the count i the user presses 1 from the menu again?
+        // This option works, but once the count gets to 0, there are no prompts after zero... so how do you restart the count after the user presses 1 from the menu again?
         // Random r = new Random();
         // while (_prompts.Count > 0)
         // {
@@ -42,11 +44,23 @@ public class PromptGenerator
         // _uniquePrompts.RemoveAt(i);
         // return prompt;
 
-        // Try to combine the two:
-        Random r = new Random();
-        
-    }
-}
+        // Tried to combine the two: DOESN'T WORK
+    //     Random r = new Random();
+    //     List<string> _uniquePrompts = new List<string>(_prompts);
+    //     if (_uniquePrompts.Count == 0)
+    //     {
+    //         _uniquePrompts = new List<string>(_prompts);
+    //     }
+    //     while (_prompts.Count >= 0)
+    //     {
+    //         int i = r.Next(_uniquePrompts.Count);
+    //         string selectedPrompt = _uniquePrompts[i];
+    //         _uniquePrompts.RemoveAt(i);
+    //         return selectedPrompt;
+    //     }
+    // return "";        
+//     }
+// }
 // https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-9.0
 // https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.removeat?view=net-9.0
 // https://learn.microsoft.com/en-us/answers/questions/1661541/add-items-to-list-randomly
