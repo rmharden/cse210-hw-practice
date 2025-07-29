61c470a5bc2a59ebe2acf0331ea722e4c9c0745a
rmharden <har22079@byui.edu>
2025-07-29 18:02:15 -0400

    Testing the code

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -3,28 +3,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        string _userResponse = "";
+        Console.Clear();
+        // string _userResponse = "";
 
-        while (_userResponse.ToLower() != "quit")
-        {
-            Console.Clear();
+        // while (_userResponse.ToLower() != "quit")
+        // {
+        //     Console.Clear();
 
-            // test code:
-            Reference r1 = new Reference("John", 3, 16);
-            Console.WriteLine(r1.GetDisplayText());
+        //     // test code:
+        //     Reference r1 = new Reference("John", 3, 16);
+        //     Console.WriteLine(r1.GetDisplayText());
 
-            Reference r2 = new Reference("Proverbs", 3, 5, 6);
-            Console.WriteLine(r2.GetDisplayText());
+        //     Reference r2 = new Reference("Proverbs", 3, 5, 6);
+        //     Console.WriteLine(r2.GetDisplayText());
 
-            // test code:
-            //Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.\n");
+        //     // test code:
+        //     //Console.WriteLine("\nProverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.\n");
 
-            // test code:
-            Scripture s = new Scripture(r2, "Proverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
-            Console.WriteLine(s.GetDisplayText());
+        //     // test code:
+        //     Scripture s = new Scripture(r2, "Proverbs 3:5-6 Trust in the Lord with all thine heart and lean not unto thine own understanding; in all thy ways acknowledge him, and he shall direct thy paths.");
+        //     Console.WriteLine(s.GetDisplayText());
 
-            Console.WriteLine("Press enter to continue or type 'quit' to finish:");
-            _userResponse = Console.ReadLine();
-        }
+        //     Console.WriteLine("Press enter to continue or type 'quit' to finish:");
+        //     _userResponse = Console.ReadLine();
+        // }
     }
 }
+
+
+// https://learn.microsoft.com/en-us/dotnet/api/system.string.-ctor?view=net-9.0
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/statements/jump-statements
+// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/operators/
+// https://learn.microsoft.com/en-us/dotnet/api/system.math.min?view=net-9.0
+// https://learn.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1.removeat?view=net-9.0
