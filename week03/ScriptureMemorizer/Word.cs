26d59958c8a7f8058ce297ce6036d52e79a1a529
rmharden <har22079@byui.edu>
2025-07-31 21:01:52 -0400

    wrote stubs in the Word class.

--- week03/ScriptureMemorizer/Word.cs
+++ week03/ScriptureMemorizer/Word.cs
@@ -2,11 +2,33 @@
 
 public class Word
 {
-    string _text;
-    List<Word> _words;
+    private string _text;
+    private bool _isHidden;
 
     public Word(string text)
     {
-        
+        _text = text;
+        _isHidden = false;
+    }
+
+    public void Hide()
+    {
+
+    }
+
+    public void Show()
+    {
+        // I still am not sure where to put this.
+    }
+
+    public bool IsHidden()
+    {
+        // if statement loop?
+        return false;
+    }
+
+    public string GetDisplayText()
+    {
+        return _text;
     }
 }
