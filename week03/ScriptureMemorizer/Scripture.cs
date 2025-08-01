e294810f75170fe07da92f749a235c109825390f
rmharden <har22079@byui.edu>
2025-07-31 21:45:58 -0400

    split the text and created new word objects. added the new word objects to the list

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -8,7 +8,16 @@ public class Scripture
         Reference _reference = reference;
         List<Word> _words = new List<Word>();
 
+        // split the words in text and store each as a word object in the list _words.
 
+        // Split and loop through each word.
+        // Create a word object and out them into _words.
+
+        foreach (string w in text.Split(" "))
+        {
+            Word aWord = new Word(w);
+            _words.Add(aWord);
+        }
     }
 
     public void HideRandomWords(int numberToHide)
