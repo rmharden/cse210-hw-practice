8a39028f73472aae898d72b9a5e76999342068d2
rmharden <har22079@byui.edu>
2025-07-31 21:25:07 -0400

    Wrote test code in the main program for a scripture object.

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -6,8 +6,12 @@ class Program
     {
         Reference r1 = new Reference("John", 3, 16);
         Console.WriteLine(r1.GetDisplayText());
+        Scripture s1 = new Scripture(r1, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        Console.WriteLine();
 
         Reference r2 = new Reference("Proverbs", 3, 5, 6);
         Console.WriteLine(r2.GetDisplayText());
+        Scripture s2 = new Scripture(r2, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. In all thy ways, acknowledge him, and he shall direct thy paths.");
+        Console.WriteLine();
     }
 } 
