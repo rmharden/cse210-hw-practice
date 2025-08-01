58616ee824b610bdd371921e4f74bbc98e18cdeb
rmharden <har22079@byui.edu>
2025-07-31 21:20:41 -0400

    Wrote stubs for the scripture class.

--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -3,5 +3,28 @@ public class Scripture
     private Reference _reference;
     private List<Word> _words;
 
-    
+    public Scripture(Reference reference, string text)
+    {
+        Reference _reference = reference;
+        List<Word> _words = new List<Word>();
+
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random r = new Random();
+    }
+
+    public string GetDisplayText()
+    {
+        // Is this a if statement or a loop?
+        return "";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        // I am pretty sure this is an if statement.
+        return false;
+    }
 }
